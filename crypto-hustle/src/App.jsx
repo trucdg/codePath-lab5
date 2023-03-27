ac88eaac491e1a90acb679a6c6b2d51c01af17c9
Truc Duong <trucdg@bu.edu>
2023-03-27 16:12:22 -0400

    use UseEffect to fetch for crypto data of all coins of blockchain type whenever the page reload

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist"
+      );
+
+      const json = await response.json();
+      console.log(json);
+      setList(json);
+    };
+
+    fetchAllCoinData().catch(console.error);
+  }, []);
 
   return (
-    <div className="App">
-      <h1> Hello world</h1>
+    <div className="whole-page">
+      <h1> My Crypto List</h1>
+      {console.log(list.Data)}
+      <ul>
+        {list &&
+          Object.entries(list.Data).map(([coin]) =>
+            list.Data[coin].PlatformType == "blockchain" ? (
+              <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+            ) : null
+          )}
+      </ul>
     </div>
   );
 }
