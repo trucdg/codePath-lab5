1faaf661ee5b4fe822e66d5b5bbaee3d6677068e
Truc Duong <trucdg@bu.edu>
2023-03-27 16:13:33 -0400

    initialize the CoinInfo Component

---
+++ crypto-hustle/src/components/CoinInfo.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+const CoinInfo = () => {
+  return (
+    <div>
+      <h1> Coin info</h1>
+    </div>
+  );
+};
+
+export default CoinInfo;
