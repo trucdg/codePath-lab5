1c31a3ab0521b042d26ef7a5a7fbb459c59a3488
Truc Duong <trucdg@bu.edu>
2023-04-01 14:12:19 -0400

    initialize the CryptoScam component which has the scamlist state variable

--- crypto-hustle/src/components/SideNav.jsx
+++ crypto-hustle/src/components/SideNav.jsx
@@ -4,6 +4,10 @@ const SideNav = () => {
   return (
     <div className="sideNav">
       <h1>here is my side nav bar</h1>
+      <p>
+        Here is a list of coins and platforms involved in recent crypto-related
+        scams:
+      </p>
     </div>
   );
 };
